d70cf056ed18451aabaf1aaa736e2935f6b117c7
dereksmilee <choss35@gmail.com>
2023-04-18 04:04:11 +0900

    first commit

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount  = 1000;
+    @Override
+    public int discount(Member memeber, int price) {
+        if(memeber.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        }
+        else{
+            return 0;
+
+        }
+    }
+}
+
